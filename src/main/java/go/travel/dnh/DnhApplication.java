612da1b6fa2f006953c736981748e9a43f69caec
deohyunpark <qwertyshsh@naver.com>
2023-02-07 17:28:09 +0900

    02/07 프로젝트 초기 설정 완료

---
+++ src/main/java/go/travel/dnh/DnhApplication.java
@@ -0,0 +1,13 @@
+package go.travel.dnh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/go/travel/dnh/DnhApplicationTests.java
@@ -0,0 +1,13 @@
+package go.travel.dnh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
