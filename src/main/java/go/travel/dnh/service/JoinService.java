d60932e5b21bc3b72e3f33c2b94d8fcc720abe02
narapark <skfk6352@naver.com>
2023-02-10 00:59:39 +0900

    joinServiceImpl 분리

--- src/main/java/go/travel/dnh/service/JoinService.java
+++ src/main/java/go/travel/dnh/service/JoinService.java
@@ -1,19 +1,10 @@
 package go.travel.dnh.service;
 
 import go.travel.dnh.domain.member.MemberDTO;
-import go.travel.dnh.mapper.MemberJoinMapper;
-import go.travel.dnh.repository.MemberJoinRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import go.travel.dnh.validation.MemberjoinForm;
 
-@Service
-@RequiredArgsConstructor
-public class JoinService {
+public interface JoinService {
 
-    private final MemberJoinRepository memberJoinRepository;
-
-    public void joinMember(MemberDTO memberDTO){
-        memberJoinRepository.joinMember(memberDTO);
-    }
+    public void joinMember(MemberjoinForm form);
 
 }
--- src/main/java/go/travel/dnh/validation/MemberjoinForm.java
+++ src/main/java/go/travel/dnh/validation/MemberjoinForm.java
@@ -6,6 +6,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.Date;
 
 @Getter
@@ -14,12 +17,25 @@ import java.util.Date;
 @AllArgsConstructor
 public class MemberjoinForm {
 
+    //@NotBlank
+    @Email
     private String mem_id;
+
+    //@NotBlank
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$\n")
     private String mem_pwd;
+
+    //@NotBlank
+    @Pattern(regexp = "^[가-힣]{2,5}$")
     private String mem_name;
+
+    //@NotBlank
+    @Pattern(regexp = "^[0-9]{10,11}$")
     private String mem_phone;
     private String mem_gender;
+
     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    //@Pattern(regexp = "^(19[0-9][0-9]|20\\d{2})-(0[0-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$")
     private Date mem_birth;
 
 }
