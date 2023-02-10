a96b4872e2c66c3d33f1c2ebcb86e38176c216e1
narapark <skfk6352@naver.com>
2023-02-11 05:29:01 +0900

    회원리스트 생성

---
+++ src/main/java/go/travel/dnh/mapper/MemberAdminMapper.java
@@ -0,0 +1,13 @@
+package go.travel.dnh.mapper;
+
+import go.travel.dnh.domain.member.MemberDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface MemberAdminMapper {
+
+    //회원 전체목록 조회
+    public List<MemberDTO> getMemberList();
+}
---
+++ src/main/java/go/travel/dnh/service/MemberAdminService.java
@@ -0,0 +1,10 @@
+package go.travel.dnh.service;
+
+import go.travel.dnh.domain.member.MemberDTO;
+
+import java.util.List;
+
+public interface MemberAdminService {
+
+    public List<MemberDTO> getMemberList();
+}
