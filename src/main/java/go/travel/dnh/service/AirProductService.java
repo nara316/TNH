276f058bebc4b5bd1bcd801cd6f65b7b54a5d612
narapark <skfk6352@naver.com>
2023-02-24 19:56:27 +0900

    항공권 예약 합치기

--- src/main/java/go/travel/dnh/service/AirProductService.java
+++ src/main/java/go/travel/dnh/service/AirProductService.java
@@ -1,11 +1,14 @@
 package go.travel.dnh.service;
 
+import go.travel.dnh.domain.User.LoginUser;
 import go.travel.dnh.domain.air.AirProductDTO;
 import go.travel.dnh.domain.air.AirportDTO;
 import go.travel.dnh.domain.air.PagingResponse;
 import go.travel.dnh.domain.air.SearchDTO;
 import go.travel.dnh.domain.reservation.AirReservationDTO;
 import go.travel.dnh.domain.reservation.ReservationDetail;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import java.util.List;
 
@@ -33,7 +36,7 @@ public interface AirProductService {
 
     AirProductDTO readRes(Integer ano);
 
-    void reservation(AirReservationDTO dto, ReservationDetail detail);
+    void reservation(AirReservationDTO dto, ReservationDetail detail, @AuthenticationPrincipal LoginUser loginUser, Authentication authentication);
 
 
 
