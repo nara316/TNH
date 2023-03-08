b36479cca996bbebca7921e7dc9a64bff8b441a3
narapark <skfk6352@naver.com>
2023-03-08 20:45:47 +0900

    bookingList 컨트롤러 이동

--- src/main/java/go/travel/dnh/controller/MemberInfoController.java
+++ src/main/java/go/travel/dnh/controller/MemberInfoController.java
@@ -3,8 +3,13 @@ package go.travel.dnh.controller;
 import go.travel.dnh.domain.User.LoginUser;
 import go.travel.dnh.domain.User.updateForm;
 import go.travel.dnh.domain.member.MemberDTO;
+import go.travel.dnh.domain.pay.PayDTO;
+import go.travel.dnh.domain.pay.RefundDTO;
+import go.travel.dnh.domain.reservation.AirReservationListDTO;
 import go.travel.dnh.service.JoinService;
 import go.travel.dnh.service.MemberLoginServiceImpl;
+import go.travel.dnh.service.PaymentService;
+import go.travel.dnh.service.ReservationService;
 import go.travel.dnh.validation.MemberjoinForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -19,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -28,6 +34,8 @@ public class MemberInfoController {
 
     private final MemberLoginServiceImpl memberLoginService;
     private final JoinService joinService;
+    private final ReservationService reservationService;
+    private final PaymentService paymentService;
 
 
     @GetMapping("/mypage")
@@ -57,6 +65,39 @@ public class MemberInfoController {
         memberLoginService.deleteMember(deleteUser);
         memberLoginService.logout(request, response);
         return true;
+    }
+
+    /*마이페이지 내 예약리스트*/
+    @GetMapping("/bookingList")
+    public String bookingList(@AuthenticationPrincipal LoginUser loginUser, Authentication authentication, Model model) {
+
+        MemberDTO memberDTO = memberLoginService.findMember(loginUser,authentication);
+        List<AirReservationListDTO> revList = reservationService.selectMyRes(loginUser,authentication);
+
+        model.addAttribute("memberDTO", memberDTO);
+        model.addAttribute("revList", revList);
+
+        return "order/bookingList";
+    }
+
+    /*예약상세*/
+    @GetMapping("/bookingList/{rno}")
+    public String showRevDetail(@PathVariable("rno") Long rno, Model model) {
+        AirReservationListDTO revDto = reservationService.getReservation(rno);
+        AirReservationListDTO revRoundDto = reservationService.getReservationRound(rno);
+        List<AirReservationListDTO> revDtDto = reservationService.getReservationDetail(rno);
+        PayDTO payDTO = paymentService.readPay(paymentService.readPno(rno));
+
+        if(payDTO !=null){
+            RefundDTO refundDTO = paymentService.readRefund(payDTO.getPno());
+            model.addAttribute("rflist",refundDTO);
+        }
+
+        model.addAttribute("list", revDto);
+        model.addAttribute("rolist",revRoundDto);
+        model.addAttribute("listDetail", revDtDto);
+        model.addAttribute("plist", payDTO);
+        return "order/bookingDetail";
     }
 
 }
