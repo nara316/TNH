41d693be53f49ccb78d750a00e50b69cfdf9fbe4
hyo123 <eater_hj@naver.com>
2023-03-08 23:16:16 +0900

    검색결과 페이징 수정

--- src/main/java/go/travel/dnh/controller/HomeController.java
+++ src/main/java/go/travel/dnh/controller/HomeController.java
@@ -1,8 +1,11 @@
 package go.travel.dnh.controller;
 
+import go.travel.dnh.domain.air.AirportDTO;
+import go.travel.dnh.domain.air.SearchDTO;
 import go.travel.dnh.domain.notice.NoticeDTO;
 import go.travel.dnh.domain.notice.NoticePageResponse;
 import go.travel.dnh.domain.notice.NoticeSearchDTO;
+import go.travel.dnh.service.AirProductService;
 import go.travel.dnh.service.NoticeBoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -11,17 +14,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/")
 public class HomeController {
 
     private final NoticeBoardService noticeBoardService;
+    private final AirProductService airProductService;
 
     @GetMapping
-    public String index(@ModelAttribute("params") NoticeSearchDTO params, Model model){
+    public String index(@ModelAttribute("params") NoticeSearchDTO params,@ModelAttribute("sch") final SearchDTO sch, Model model){
         NoticePageResponse<NoticeDTO> response = noticeBoardService.boardList(params);
+        List<AirportDTO> airportList = airProductService.getListAirport();
+        model.addAttribute("airport",airportList);
         model.addAttribute("response",response );
         return "index";
     }
+
 }
